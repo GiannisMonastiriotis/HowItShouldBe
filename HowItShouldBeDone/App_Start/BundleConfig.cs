aa5474b0fb128c438c5a84128eacd580ac6583b0
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-14 00:45:56 +0300

    Added BootstrapSuperHero.Css

--- HowItShouldBeDone/App_Start/BundleConfig.cs
+++ HowItShouldBeDone/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace HowItShouldBeDone
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapSuperHero.css",
                       "~/Content/site.css"));
         }
     }
