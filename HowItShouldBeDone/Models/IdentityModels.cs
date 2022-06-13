62684a1443eb867bc2d833dd024a9675039bf980
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-14 01:12:25 +0300

    DbsetsJustBeforeAddInitialMigration

--- HowItShouldBeDone/Models/IdentityModels.cs
+++ HowItShouldBeDone/Models/IdentityModels.cs
@@ -25,6 +25,10 @@ namespace HowItShouldBeDone.Models
         {
         }
 
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
