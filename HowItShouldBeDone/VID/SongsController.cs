128151fe78a40c3a4b3bb933f3be746230799bcd
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-14 17:20:08 +0300

    Added Delete

--- HowItShouldBeDone/VID/SongsController.cs
+++ HowItShouldBeDone/VID/SongsController.cs
@@ -62,11 +62,22 @@ namespace HowItShouldBeDone.VID
             try
             {
                 song.Youtube = $"https://www.youtube.com/embed/{song.Youtube}";
-                _context.Songs.Add(song);
+                if (song.ID == 0)
+                {
+                    _context.Songs.Add(song);
+                }
+                else
+                {
+                    var songInDb = _context.Songs.SingleOrDefault(s => s.ID == song.ID);
+                    songInDb.Title = song.Title;
+                    songInDb.Youtube = song.Youtube;
+                    songInDb.AlbumId = song.AlbumId;
+                }
+                
                 if (!ModelState.IsValid)
                 {
                     var viewModel = new SongFormViewmodel() { song = new Song(),Albums = _context.Albums.ToList()};
-                    return View("SongFrom", viewModel);
+                    return View("SongForm", viewModel);
                 }
                 _context.SaveChanges();
             }
@@ -84,24 +95,63 @@ namespace HowItShouldBeDone.VID
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            //GET THE SONGS FROM THE DATABASE
             var song = _context.Songs
                 .Include(s=> s.Album)
                 .SingleOrDefault(s => s.ID == id);
+
+            //CHECK IF THE SONG IS NULL
             if(song == null)
             {
                 return HttpNotFound();
             }
-
+            //GET THE ALBUM FROM THE DATABASE
             var albums = _context.Albums.ToList();
-
+            //INSTANTIATE VIEW MODEL AND FILL WITH ALBUMS AND SONG QUERIES WE JUST DID
             var viewModel = new SongFormViewmodel()
             {
                 song = song,
                 Albums = albums
             };
 
-            return View("SongFrom", viewModel);
+            return View("SongForm", viewModel);
 
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var song = _context.Songs
+                .Include(s => s.Album)
+                .SingleOrDefault(s => s.ID == id);
+
+            if(song == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(song);
+        }
+
+        [HttpPost]
+
+        public ActionResult DeleteConfirmed(int id)
+        {
+
+            var song = _context.Songs.SingleOrDefault(s => s.ID == id);
+            _context.Songs.Remove(song);
+
+            if(song == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.SaveChanges();
+            return Redirect("Index");
         }
     }
 }
