17b3054d09032091790a6617a521b10757cc117f
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-14 12:27:03 +0300

    Added youtube property to songs

---
+++ HowItShouldBeDone/Migrations/202206140923079_Youtubeurl.cs
@@ -0,0 +1,18 @@
+namespace HowItShouldBeDone.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Youtubeurl : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Songs", "Youtube", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Songs", "Youtube");
+        }
+    }
+}
